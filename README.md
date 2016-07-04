8b798c94d13820e8e96fd8a5bd12d06d08942d88
tanbingjian <tanbingjian@maoguang.org>
2016-07-04 14:53:00 +0800

    init

---
+++ README.md
@@ -0,0 +1,6 @@
+
+Maven 打包尝试
+
+包括不带依赖打包和带依赖打包
+
+详细信息请查看[Maven打包记录](https://teclan.github.io/2016/07/04/Mavn%E6%89%93%E5%8C%85%E8%AE%B0%E5%BD%95/)
---
+++ src/main/java/teclan/mavenpackage/App.java
@@ -0,0 +1,13 @@
+package teclan.mavenpackage;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
